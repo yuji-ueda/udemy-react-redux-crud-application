b77a73fdc1f6c3780993c9fa814ada370422df1f
yuji-ueda <yt.works12@gmail.com>
2020-05-23 01:10:57 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 
 
@@ -6,7 +7,7 @@ const App = () => {
   const profiles = [
     {name: "Taro", age: 10,},
     {name: "Hanako", age: 5,},
-    {name: "NoName"},
+    {name: "NoName", age: 3,},
   ]
   return(
     <div>
@@ -23,8 +24,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 }
 
 export default App;
