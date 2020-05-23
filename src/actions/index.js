41530f314368fcc59cd9bbdd1d27b92f308cd4b6
yuji-ueda <yt.works12@gmail.com>
2020-05-23 10:03:55 +0900

    implement action and actin creater

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+
+export const increment = () => ({
+  type: INCREMENT,
+})
+
+export const increment = () => ({
+  type: DECREMENT,
+})
+
+
+
+
