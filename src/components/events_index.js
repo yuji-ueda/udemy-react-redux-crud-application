5e4b84396f3a811318207b1a587904e6f7e12d5e
yuji-ueda <yt.works12@gmail.com>
2020-05-24 17:30:45 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux"
 import  _ from "lodash"
+import { Link } from "react-router-dom"
 
 import { readEvents } from "../actions"
 
@@ -22,25 +23,29 @@ class EventsIndex extends Component {
   render() {
 
     return(
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderEvents()}
-          </tbody>
-        </table>
+        <React.Fragment>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderEvents()}
+            </tbody>
+          </table>
+          
+          <Link to = "/events/new">New Event</Link>
+        </React.Fragment>
     )
   }
 }
 
 const mapStateToProps = state => ({ events: state.events })
-const mapDisPatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({
   readEvents: () => dispatch(readEvents()),
 })
 
-export default connect(mapStateToProps, mapDisPatchToProps)(EventsIndex)
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk"
+import thunk from "redux-thunk";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import './index.css';
 import reducer from "./reducers"
 import EventsIndex from "./components/events_index.js"
+import EventsNew from "./components/events_new.js"
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
